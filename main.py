020a816d033cc5dcdece221cea22322fea9d1ae5
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2023-01-04 22:33:50 +0200

    Added some changes

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ icon = PhotoImage(file='calculator.png')
 Window.iconphoto(True,icon)
 Window.config(background="black")
 
-def appMode():
+e = Entry(Window, width = 360 , bg ="Black", fg="white")
+e.pack()
+
+def lightMode():
     Window.config(background="white")
+    
+
+def equalbutton():
+    label = Label(Window, text=e.get(),fg='white',bg='black',activeforeground='white',activebackground='black',bd=0)
+    label.pack()
 
 
-button = Button(Window,text="Light mode",command=appMode,font=('comic sans',10),fg='white',bg='black',activeforeground='white',activebackground='black',bd=0)
+button = Button(Window,text="Mode",command=lightMode,font=('comic sans',10),
+                fg='white',bg='black',activeforeground='white',activebackground='black',bd=0)
 button.pack()
-
+equal = Button(Window, text="=", command=equalbutton,fg='white',bg='black',activeforeground='white',activebackground='black',bd=0)
+equal.pack()
 Window.mainloop() #Listen for events and display our window
 
 
